109fd1711c164bccff94cd24f270691c7518657b
JinYongJJang <43266674+JinYongJJang@users.noreply.github.com>
2023-04-22 16:52:38 +0900

    코드 길어지면 import export

--- coding_apple_part2/src/App.js
+++ coding_apple_part2/src/App.js
@@ -2,47 +2,53 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import {Navbar, Container, Nav} from 'react-bootstrap'
 import Row from 'react-bootstrap/Row';
- import Col from 'react-bootstrap/Col';
+import Col from 'react-bootstrap/Col';
+import data from './data.js'
+import { useState } from 'react'
 
 function App() {
-  return (
-    <div className="App">
-      <Navbar bg="dark" variant="dark">
-        <Container>
-          <Navbar.Brand href="#home">Navbar</Navbar.Brand>
-          <Nav className="me-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#features">Features</Nav.Link>
-            <Nav.Link href="#pricing">Pricing</Nav.Link>
-          </Nav>
-        </Container>
-      </Navbar>
+    let [shoes] = useState(data);
 
-      <div>
-        <img style={{height:"300px", "background-size":"cover"}} src="./bg.png"/ >
-      </div>
+    return (
+        <div className="App">
+            <Navbar bg="dark" variant="dark">
+            <Container>
+                <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+                <Nav className="me-auto">
+                    <Nav.Link href="#home">Home</Nav.Link>
+                    <Nav.Link href="#features">Features</Nav.Link>
+                    <Nav.Link href="#pricing">Pricing</Nav.Link>
+                </Nav>
+            </Container>
+            </Navbar>
 
-      <Container>
-        <Row>
-          <Col sm>
-            <img width="80%" src="shoes1.jpg"/ >
-            <h4>상품명</h4>
-            <p>상품정보</p>
-          </Col>
-          <Col sm>
-            <img width="80%" src="shoes2.jpg"/ >
-            <h4>상품명</h4>
-            <p>상품정보</p>
-          </Col>
-          <Col sm>
-            <img width="80%" src="https://codingapple1.github.io/shop/shoes3.jpg"/ >
-            <h4>상품명</h4>
-            <p>상품정보</p>
-          </Col>
-        </Row>
-      </Container>
-    </div>
-  );
+            <div>
+                <img style={{height:"300px", "background-size":"cover"}} src="./bg.png"/ >
+            </div>
+
+            <Container>
+                <Row>
+                    {
+                        shoes.map(function(d, i){
+                            return(
+                                <Item src={"shoes"+ (i+1) +".jpg"} title={d.title} price={d.price}></Item>
+                            )
+                        })
+                    }
+
+                </Row>
+            </Container>
+        </div>
+    );
 }
 
+function Item(props){
+    return (
+        <Col sm>
+            <img width="80%" src={props.src}/ >
+            <h4>{props.title}</h4>
+            <p>{props.price}</p>
+        </Col>
+    )
+}
 export default App;
